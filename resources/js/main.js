f783e56e692573d2dba93f155777c7070438a377
王 美丽 <wywang@outekiMacBook-Pro.local>
2018-05-28 23:39:35 -0700

    fix navbar problem

--- resources/js/main.js
+++ resources/js/main.js
@@ -8,17 +8,18 @@ $(document).ready(function(){
     easing: 'swing',
     speed: 1000
   });
-});
 
-$('.navbar').ready(function(){
+
   var x = 0;
-  var y = $(this).scrollTop();
-  if (y - x > 50) {
-    var z = $('.navbar').css('height');
-    $('.navbar').animate({top: '-' + z}, 150);
-    x === y;
-  } else {
-    $('.navbar').animate({top: 0}, 150);
-    x === y;
-  }
+  $(window).scroll(function(){
+    var y = $(this).scrollTop();
+    if (y - x > 50){
+      var z = $('.navbar').css('height');
+      $('.navbar').animate({top: '-' + z}, 150);
+      x = y;
+    } else if (x - y > 50){
+      $('.navbar').animate({top: '0px'}, 150);
+      x = y;
+    }
+  });
 });
